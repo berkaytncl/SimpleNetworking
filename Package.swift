bbdb82d32a9857803697e36098b3f35e6805e531
Berkay Tuncel <berkaytncl98@gmail.com>
2024-05-16 12:04:53 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SimpleNetworking",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "SimpleNetworking",
+            targets: ["SimpleNetworking"]),
+    ],
+    targets: [
+        .target(
+            name: "SimpleNetworking")
+    ]
+)
---
+++ Sources/SimpleNetworking/SimpleNetworking.swift
@@ -0,0 +1,43 @@
+import Foundation
+import Combine
+
+public final class NetworkingManager {
+    
+    public init() {}
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🔥] Bad response from URL: \(url)"
+            case .unknown: return "[⚠️] Unknown error occured."
+            }
+        }
+    }
+    
+    public static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .retry(3)
+            .eraseToAnyPublisher()
+    }
+    
+    private static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    public static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
